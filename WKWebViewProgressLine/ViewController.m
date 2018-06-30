092f75408284348b0f2eb7d560937b1ae68cbfd5
wanliju <hailangjwl@163.com>
2018-06-30 17:35:24 +0800

    add progress line in webView

--- WKWebViewProgressLine/ViewController.m
+++ WKWebViewProgressLine/ViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "ViewController.h"
+#import <WebKit/WebKit.h>
 
 @interface ViewController ()
+
+@property (nonatomic,weak)WKWebView * webView;
+@property (nonatomic,weak)CALayer * progressLayer;
 
 @end
 
@@ -16,14 +20,53 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    WKWebView * webView = [[WKWebView alloc] initWithFrame:self.view.bounds];
+    [self.view addSubview:webView];
+    self.webView = webView;
+    // 添加属性监听
+    [webView addObserver:self forKeyPath:@"estimatedProgress" options:NSKeyValueObservingOptionNew context:nil];
+    
+    // 进度条
+    UIView * progress = [[UIView alloc] initWithFrame:CGRectMake(0, 64, CGRectGetWidth(self.view.frame), 3)];
+    progress.backgroundColor = [UIColor clearColor];
+    [self.view addSubview:progress];
+    
+    // 隐式动画
+    CALayer * layer = [CALayer layer];
+    layer.frame = CGRectMake(0, 0, 0, 3);
+    layer.backgroundColor = [UIColor blueColor].CGColor;
+    [progress.layer addSublayer:layer];
+    self.progressLayer = layer;
+    
+    [webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"https://www.baidu.com"]]];
+
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSKeyValueChangeKey,id> *)change context:(void *)context{
+    if ([keyPath isEqualToString:@"estimatedProgress"]) {
+        NSLog(@"change == %@",change);
+        self.progressLayer.opacity = 1;
+        //不要让进度条倒着走...有时候goback会出现这种情况
+        if ([change[NSKeyValueChangeNewKey] floatValue] < [change[NSKeyValueChangeOldKey] floatValue]) {
+            return;
+        }
+        self.progressLayer.frame = CGRectMake(0, 0, self.view.bounds.size.width * [change[NSKeyValueChangeNewKey] floatValue], 3);
+        if ([change[NSKeyValueChangeNewKey] floatValue] == 1) {
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(.2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                self.progressLayer.opacity = 0;
+            });
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(.8 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                self.progressLayer.frame = CGRectMake(0, 0, 0, 3);
+            });
+        }
+    }else {
+        [super observeValueForKeyPath:keyPath ofObject:object change:change context:context];
+    }
 }
 
+- (void)dealloc {
+    [self.webView removeObserver:self forKeyPath:@"estimatedProgress"];
+}
 
 @end
